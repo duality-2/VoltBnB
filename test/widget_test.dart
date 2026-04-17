e799b251ad49c818bbfeed84e944ba9a75a767fb
HarshalChugwani2005 <2023.harshal.chugwani@ves.ac.in>
2026-04-17 12:37:57 +0530

    Initial Commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:evshare/main.dart';
+import 'package:voltbnb/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ProviderScope(child: EVShareApp()));
+    await tester.pumpWidget(const ProviderScope(child: VoltBnBApp()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
